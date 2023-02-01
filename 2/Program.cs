f18a8f8500f48b53d062cab461aa97ce4b4c0687
Frukt <88rum@mail.ru>
2023-02-01 11:11:05 +0300

    создали задачу 2

---
+++ 2/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+int summa = 0;
+int SumNumber(int m, int n)
+{
+    if (m < n)
+    {
+        summa = summa + m;
+        SumNumber(m + 1, n);
+        return summa + n;
+    }
+    else if (m > n)
+    {
+        summa = summa + n;
+        SumNumber(m, n + 1);
+        return summa + m;
+    }
+    else
+    {
+        return summa;
+    }
+}
+Console.WriteLine($"Сумма элементов в промежутке равна: {SumNumber(m, n)}");
